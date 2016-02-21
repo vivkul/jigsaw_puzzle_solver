cd9c13994289e4d74e5ec1becbe28a108e90cf39
Viveka Kulharia <vivekakulharia@gmail.com>
2016-02-22 03:03:48 +0530

    converted to usable form

--- image_segment.py
+++ image_segment.py
@@ -1,31 +1,40 @@
 import Image
 import numpy as np
 import sys
+import glob
 
-
-def merge_image(m,n,image_width, image_height):
+def merge_image(m,n,image_width, image_height,I):
+	name=I[:-5]
 	t=n*m
-	merge_order=np.random.permutation(t)
-	final_image=Image.new('RGB',(image_width,image_height))
+	perm_list=set()
+	while len(perm_list)<6:
+		temp=range(t)
+		np.random.shuffle(temp)
+		perm_list.add(tuple(temp))
 	w_chunk=image_width/m
 	h_chunk=image_height/n
-	k=0
-	img_ext=".jpg"
-	order_txt=open('final.txt','a')
-	for i in range(1,n+1):
-		lt_h=h_chunk*(i-1)
-		for j in range(1,m+1):
-			lt_w=w_chunk*(j-1)
-			order_txt.write(str(merge_order[k]+1))
-			order_txt.write("\n")
-			part_name=str(merge_order[k]+1)
-			part_name+=img_ext
-			part_im=Image.open(part_name)
-			final_image.paste(part_im,(lt_w,lt_h))
-			k=k+1
-	final_image.save("final.jpg")
+	img_ext=".JPEG"
+	while len(perm_list)>0:
+		k=0
+		final_image=Image.new('RGB',(image_width,image_height))
+		merge_order=perm_list.pop()
+		order_txt=open(name+"_"+str(len(perm_list))+'.txt','a')
+		for i in range(1,n+1):
+			lt_h=h_chunk*(i-1)
+			for j in range(1,m+1):
+				lt_w=w_chunk*(j-1)
+				order_txt.write(str(merge_order[k]+1))
+				order_txt.write("\n")
+				part_name=str(merge_order[k]+1)
+				part_name+=img_ext
+				part_im=Image.open(part_name)
+				final_image.paste(part_im,(lt_w,lt_h))
+				del part_im
+				k=k+1
+		final_image.save(name+"_"+str(len(perm_list))+".JPEG")
+		order_txt.close()
 
-def split_image(im,m,n):
+def split_image(im,m,n,I):
 	w,h=im.size
 	print w
 	print h
@@ -36,7 +45,7 @@ def split_image(im,m,n):
 	t=n*m+1
 	im_number=range(1,t)
 	k=0
-	img_ext=".jpg"
+	img_ext=".JPEG"
 	for i in range(1,n+1):
 		lt_h=h_chunk*(i-1)
 		for j in range(1,m+1):
@@ -45,16 +54,19 @@ def split_image(im,m,n):
 			split_name+=img_ext
 			im.crop((lt_w,lt_h,lt_w+w_chunk,lt_h+h_chunk)).save(split_name)
 			k=k+1
-	merge_image(m,n,w,h)		
+	merge_image(m,n,w,h,I)		
 
-def image_read():
-	im=Image.open("test.jpg")
-	m=3
-	n=3
-	split_image(im,m,n)
+def image_read(I):
+	im=Image.open(I)
+	m=2
+	n=2
+	split_image(im,m,n,I)
+	del im
 
 def main():
-	image_read()
+	Images=glob.glob('*.JPEG')
+	for I in Images:
+		image_read(I)
 
 if __name__ == "__main__":
 	main()
