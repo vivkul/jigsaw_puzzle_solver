b8b5a1b5de14c68096cb171de0ef0227450a77ee
Viveka Kulharia <vivekakulharia@gmail.com>
2016-04-08 10:38:31 +0530

    mrf_Imagenet.py can now handle odd pixel image

--- mrf_Imagenet.py
+++ mrf_Imagenet.py
@@ -9,30 +9,59 @@ per_index=list(enumerate(''.join(p) for p in permutations('1234')))
 def get_energy_quad(i,j,orientation,im):
 	image_width,image_height=im.size
 	m=n=2
-	w_chunk=image_width/m
-	h_chunk=image_height/n
+	w_chunk1=image_width/m
+	w_chunk2=image_width-w_chunk1
+	h_chunk1=image_height/n
+	h_chunk2=image_height-h_chunk1
 	im_matrix=np.asarray(im)
 	if orientation==0:
-		x_top=(i/m)*h_chunk
-		x_bot=(i/m+1)*h_chunk-1
-		x_right=(i%m+1)*w_chunk-1
-		y_top=(j/m)*h_chunk
-		y_bot=(j/m+1)*h_chunk-1
-		y_left=(j%m)*w_chunk
+		x_top=(i/m)*h_chunk1
+		if i/m==0:
+			x_bot=h_chunk1-1
+		else:
+			x_bot=image_height-1
+		if i%m==0:
+			x_right=w_chunk1-1
+		else:
+			x_right=image_width-1
+		y_top=(j/m)*h_chunk1
+		if j/m==0:
+			y_bot=h_chunk1-1
+		else:
+			y_bot=image_height-1
+		y_left=(j%m)*w_chunk1
 		sum_=0
-		for l in range(h_chunk):
-			sum_=sum_+sum(np.square(im_matrix[x_top+l,x_right]-im_matrix[y_top+l,y_left]))
+		if im_matrix[0,0].size == 1:
+			for l in range(h_chunk):
+				sum_=sum_+np.square(im_matrix[x_top+l,x_right]-im_matrix[y_top+l,y_left])
+		else:
+			for l in range(h_chunk):
+				sum_=sum_+sum(np.square(im_matrix[x_top+l,x_right]-im_matrix[y_top+l,y_left]))
 		return sum_/(h_chunk*1.0)
 	else:
 		x_bot=(i/m+1)*h_chunk-1
-		x_left=(i%m)*w_chunk
-		x_right=(i%m+1)*w_chunk-1
-		y_top=(j/m)*h_chunk
-		y_left=(j%m)*w_chunk
-		y_right=(j%m+1)*w_chunk-1
+		if i/m==0:
+			x_bot=h_chunk1-1
+		else:
+			x_bot=image_height-1
+		x_left=(i%m)*w_chunk1
+		if i%m==0:
+			x_right=w_chunk1-1
+		else:
+			x_right=image_width-1
+		y_top=(j/m)*h_chunk1
+		y_left=(j%m)*w_chunk1
+		if j%m==0:
+			y_right=w_chunk1-1
+		else:
+			y_right=image_width-1
 		sum_=0
-		for l in range(w_chunk):
-			sum_=sum_+sum(np.square(im_matrix[x_bot,x_left+l]-im_matrix[y_top,y_left+l]))
+		if im_matrix[0,0].size == 1:
+			for l in range(w_chunk):
+				sum_=sum_+np.square(im_matrix[x_bot,x_left+l]-im_matrix[y_top,y_left+l])
+		else:
+			for l in range(w_chunk):
+				sum_=sum_+sum(np.square(im_matrix[x_bot,x_left+l]-im_matrix[y_top,y_left+l]))
 		return sum_/(w_chunk*1.0)
 
 def pre_compute_energy(im):
@@ -59,16 +88,16 @@ def image_read(I):
 	w,h=im.size
 	m=2
 	n=2
-	if w%m==0 and h%n==0:
-			pre_compute_energy(im)
-			del im
-			return find_optimal_permutation()
+	# if w%m==0 and h%n==0:
+	pre_compute_energy(im)
+	del im
+	return find_optimal_permutation()
 
-	else:
-		print "size not multiple of 2 for"
-		print I
-		del im
-		return -1
+	# else:
+	# 	print "size not multiple of 2 for"
+	# 	print I
+	# 	del im
+	# 	return -1
 
 def main():
 	Images=glob.glob('*.JPEG')
